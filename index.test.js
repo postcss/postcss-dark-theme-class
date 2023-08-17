71927bc09676900f42303ba2a700c74aec961835
NataliaNagaeva <astashkinana@gmail.com>
2023-08-17 12:58:55 +0300

    Add `preferred-color-scheme: light` support (#23)
    * add light color scheme support
    
    * fix README
    
    * separate tests for light scheme
    
    * remove an example for the light scheme from README

--- index.test.js
+++ index.test.js
@@ -10,7 +10,7 @@ function run(input, output, opts) {
   equal(result.warnings().length, 0)
 }
 
-test('replaces selectors', () => {
+test('replaces selectors - dark scheme', () => {
   run(
     `@media (prefers-color-scheme:dark) {
     html.is-a,
@@ -31,7 +31,65 @@ test('replaces selectors', () => {
   )
 })
 
-test('disables :where() of request', () => {
+test('replaces selectors - light scheme', () => {
+  run(
+    `@media (prefers-color-scheme:light) {
+    html.is-a,
+    html,
+    :root,
+    a { }
+  }`,
+    `@media (prefers-color-scheme:light) {
+    html:where(:not(.is-dark)).is-a,
+    html:where(:not(.is-dark)),
+    :root:where(:not(.is-dark)),
+    :where(html:not(.is-dark)) a { }
+  }
+    html:where(.is-light).is-a,
+    html:where(.is-light),
+    :root:where(.is-light),
+    :where(html.is-light) a { }`
+  )
+})
+
+test('replaces selectors - dark and light schemes', () => {
+  run(
+    `@media (prefers-color-scheme:dark) {
+    html.is-a,
+    html,
+    :root,
+    a { }
+  }
+  @media (prefers-color-scheme:light) {
+    html.is-a,
+    html,
+    :root,
+    a { }
+  }`,
+    `@media (prefers-color-scheme:dark) {
+    html:where(:not(.is-light)).is-a,
+    html:where(:not(.is-light)),
+    :root:where(:not(.is-light)),
+    :where(html:not(.is-light)) a { }
+  }
+    html:where(.is-dark).is-a,
+    html:where(.is-dark),
+    :root:where(.is-dark),
+    :where(html.is-dark) a { }
+  @media (prefers-color-scheme:light) {
+    html:where(:not(.is-dark)).is-a,
+    html:where(:not(.is-dark)),
+    :root:where(:not(.is-dark)),
+    :where(html:not(.is-dark)) a { }
+  }
+  html:where(.is-light).is-a,
+    html:where(.is-light),
+    :root:where(.is-light),
+    :where(html.is-light) a { }`
+  )
+})
+
+test('disables :where() of request - dark scheme', () => {
   run(
     `@media (prefers-color-scheme:dark) {
     html.is-a,
@@ -53,7 +111,29 @@ test('disables :where() of request', () => {
   )
 })
 
-test('processes inner at-rules', () => {
+test('disables :where() of request - light scheme', () => {
+  run(
+    `@media (prefers-color-scheme:light) {
+    html.is-a,
+    html,
+    :root,
+    a { }
+  }`,
+    `@media (prefers-color-scheme:light) {
+    html:not(.is-dark).is-a,
+    html:not(.is-dark),
+    :root:not(.is-dark),
+    html:not(.is-dark) a { }
+  }
+    html.is-light.is-a,
+    html.is-light,
+    :root.is-light,
+    html.is-light a { }`,
+    { useWhere: false }
+  )
+})
+
+test('processes inner at-rules - dark scheme', () => {
   run(
     `@media (prefers-color-scheme: dark) {
     @media (min-width: 500px) {
@@ -81,6 +161,39 @@ test('processes inner at-rules', () => {
     @media (min-width: 500px) {
       @media (print) {
         :where(html.is-dark) a { }
+      }
+    }`
+  )
+})
+
+test('processes inner at-rules - light scheme', () => {
+  run(
+    `@media (prefers-color-scheme: light) {
+    @media (min-width: 500px) {
+      a { }
+    }
+    @media (min-width: 500px) {
+      @media (print) {
+        a { }
+      }
+    }
+  }`,
+    `@media (prefers-color-scheme: light) {
+    @media (min-width: 500px) {
+      :where(html:not(.is-dark)) a { }
+    }
+    @media (min-width: 500px) {
+      @media (print) {
+        :where(html:not(.is-dark)) a { }
+      }
+    }
+  }
+    @media (min-width: 500px) {
+      :where(html.is-light) a { }
+    }
+    @media (min-width: 500px) {
+      @media (print) {
+        :where(html.is-light) a { }
       }
     }`
   )
@@ -133,7 +246,7 @@ test('reserve comments', () => {
   )
 })
 
-test('supports combined queries', () => {
+test('supports combined queries - dark scheme', () => {
   run(
     `@media (min-width: 60px) and (prefers-color-scheme: dark) {
     a { color: white }
@@ -146,7 +259,20 @@ test('supports combined queries', () => {
   )
 })
 
-test('supports combined queries in the middle', () => {
+test('supports combined queries - light scheme', () => {
+  run(
+    `@media (min-width: 60px) and (prefers-color-scheme: light) {
+    a { color: white }
+  }`,
+    `@media (min-width: 60px) and (prefers-color-scheme: light) {
+    :where(html:not(.is-dark)) a { color: white }
+  }@media (min-width: 60px) {
+    :where(html.is-light) a { color: white }
+  }`
+  )
+})
+
+test('supports combined queries in the middle - dark scheme', () => {
   run(
     `@media (width > 0) and (prefers-color-scheme: dark) and (width > 0) {
     a { color: white }
@@ -155,6 +281,19 @@ test('supports combined queries in the middle', () => {
     :where(html:not(.is-light)) a { color: white }
   }@media (width > 0) and (width > 0) {
     :where(html.is-dark) a { color: white }
+  }`
+  )
+})
+
+test('supports combined queries in the middle - light scheme', () => {
+  run(
+    `@media (width > 0) and (prefers-color-scheme: light) and (width > 0) {
+    a { color: white }
+  }`,
+    `@media (width > 0) and (prefers-color-scheme: light) and (width > 0) {
+    :where(html:not(.is-dark)) a { color: white }
+  }@media (width > 0) and (width > 0) {
+    :where(html.is-light) a { color: white }
   }`
   )
 })
@@ -212,7 +351,7 @@ test('changes root selector', () => {
   )
 })
 
-test('ignores already transformed rules', () => {
+test('ignores already transformed rules - dark scheme', () => {
   run(
     `@media (prefers-color-scheme: dark) {
     :root:not(.is-light) { --bg: black }
@@ -224,6 +363,22 @@ test('ignores already transformed rules', () => {
     :where(html:not(.is-light)) p { color: white }
   }
     :where(html.is-dark) p { color: white }
+  :root { --bg: white }`
+  )
+})
+
+test('ignores already transformed rules - light scheme', () => {
+  run(
+    `@media (prefers-color-scheme: light) {
+    :root:not(.is-dark) { --bg: black }
+    p { color: white }
+  }
+  :root { --bg: white }`,
+    `@media (prefers-color-scheme: light) {
+    :root:not(.is-dark) { --bg: black }
+    :where(html:not(.is-dark)) p { color: white }
+  }
+    :where(html.is-light) p { color: white }
   :root { --bg: white }`
   )
 })
